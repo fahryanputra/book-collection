03c4b4f38caa51d05ffb9e359e592ea159d212a5
fahryanputra <fahryanputra@gmail.com>
2024-01-08 10:55:08 +0700

    Fix card paragraph word wrap

--- script.js
+++ script.js
@@ -16,12 +16,22 @@ function Book(title, author, year, page, read) {
 
 // let user add book to collection
 function addBookToCollection() {
+    const titleInput = document.getElementById("bookTitle").value;
+    const authorInput = document.getElementById("bookAuthor").value;
+    const yearInput = document.getElementById("bookYear").value;
+    const pageInput = document.getElementById("bookPage").value;
+    const readInput = document.getElementById("bookRead").checked;
 
+    const newBook = new Book(titleInput, authorInput, yearInput, pageInput, readInput);
+    myCollection.push(newBook);
 };
 
 // function to create collection cards
 function createBookCards(array) {
     const cardsContainer = document.querySelector(".cards");
+
+    // clear cardsContainer child
+    cardsContainer.textContent = '';
 
     array.forEach(element => {
         // card div
@@ -31,7 +41,7 @@ function createBookCards(array) {
         // card div content
         // title
         const titleText = document.createElement("p");
-        titleText.textContent = element.title;
+        titleText.textContent = `"${element.title}"`;
         // author
         const authorText = document.createElement("p");
         authorText.textContent = element.author;
@@ -43,8 +53,7 @@ function createBookCards(array) {
         pagesText.textContent = element.page.toString();
         // read status
         const readText = document.createElement("p");
-        readText.textContent = element.read ? "Finished reading" : "Not yet read";
-        
+        readText.textContent = element.read ? "Finished reading" : "Not yet read";        
 
         // append newly created element to cards collection
         bookCard.appendChild(titleText);
@@ -62,15 +71,16 @@ addBookButton.addEventListener("click", () => {
 })
 
 submitButton.addEventListener("click", () => {
+    addBookToCollection()
+    createBookCards(myCollection);
     dialog.close();
 })
 
-// seed myCollection array
+// // seed myCollection array
 const sherlock = new Book("The Adventures of Sherlock Holmes", "Sir Arthur Conan Doyle", 1892, 307, false);
-const poirot = new Book("The Murder on the Links", "Agatha Christie", 1923, 298, false);
+// const poirot = new Book("The Murder on the Links", "Agatha Christie", 1923, 298, false);
 
-// add new object to the array
+// // add new object to the array
 myCollection.push(sherlock);
-myCollection.push(poirot);
-
 createBookCards(myCollection);
+// myCollection.push(poirot);
